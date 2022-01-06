ab03b8fdd177c6db537996aa8795de7a65bb6837
Md Arifur Rahman <rahman1193arif@gmail.com>
2022-01-06 15:01:22 +0600

    second video

--- src/Components/SecondVideo/SecondVideo.js
+++ src/Components/SecondVideo/SecondVideo.js
@@ -1,20 +1,30 @@
 import React from "react";
-import secondImage from '../../images/secondImage.png';
-import './SecondVideo.css';
+import secondImage from "../../images/secondImage.png";
+import "./SecondVideo.css";
 
 const SecondVideo = () => {
   return (
     <div className="container second-image">
-        <h1>This is Video</h1>
+      <div>
+          <p className="second-image-text">
+          <span className="second-image-first">Take A Sneak Peek And See.. </span><br/>
+          <span className="second-image-second">What Our Supper Funnel Hero's Are Saying After Using</span> <br/>
+          <span className="second-image-third">their <span className="second-image-third-span">FREE Super Funnel</span></span>
+          </p>
+        {/* <h2>Take A Sneak Peek And See..</h2>
+        <h1>What Our Supper Funnel Hero's Are Saying After Using</h1>
+        <h2>
+          their <span>FREE Super Funnel</span>
+        </h2> */}
+      </div>
       <div className="embed-responsive embed-responsive-16by9">
-      {/* <img className="second-image" src={secondImage} alt="" /> */}
+        {/* <img className="second-image" src={secondImage} alt="" /> */}
         <iframe
           className="embed-responsive-item"
           src="https://www.youtube.com/embed/tgbNymZ7vqY"
         ></iframe>
       </div>
-      </div>
-    
+    </div>
   );
 };
 
