ec9c777aba696fdb7be9090318026ce62dcf18b4
Md Arifur Rahman <rahman1193arif@gmail.com>
2022-01-07 01:27:02 +0600

    fourth section done

--- src/Components/ForthIImage/ForthIImage.js
+++ src/Components/ForthIImage/ForthIImage.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
+import "./ForthIImage.css";
+import ForthImage from "../../images/forthImage.png";
 
 const ForthIImage = () => {
-    return (
-        <div>
-            <h1>New Image Section</h1>
-        </div>
-    );
+  return (
+    <div className="container image2-section">
+      <p>
+        <span className="image2-text-first">What Are YOU Waiting For...</span>{" "}
+        <br />{" "}
+        <span className="image2-text-second">
+          Id like to rush a free copy of my brand new{" "}
+        </span>
+        <br /> Super Funnel to You,{" "}
+        <span className="image2-text-forth">ASAP!</span>
+      </p>
+      <img className="image2-responsive" src={ForthImage} alt="" />
+      <p className="image2-paragraph">
+        Like I mentioned before, <span className="image2-paragraph-first">my Super Funnel is free</span>. This works anywhere in
+        the world! <span className="image2-paragraph-first">To take this all you have to do is click below...</span>
+      </p>
+      <button className="image2-button-text">YES! I WANT THE FREE SUPER FUNNEL</button>
+
+    </div>
+  );
 };
 
 export default ForthIImage;
--- src/Components/UpperSection/VedioPlayer/VedioPlayer.js
+++ src/Components/UpperSection/VedioPlayer/VedioPlayer.js
@@ -3,35 +3,16 @@ import ReactPlayer from "react-player";
 import "./VedioPlayer.css";
 
 const VedioPlayer = () => {
-    
   return (
-   <div className="container player-bottom">
+    <div className="container player-bottom">
       <div className="embed-responsive embed-responsive-16by9">
-    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/tgbNymZ7vqY"></iframe>
-  </div>
-   </div>
+        <iframe
+          className="embed-responsive-item"
+          src="https://www.youtube.com/embed/tgbNymZ7vqY"
+        ></iframe>
+      </div>
+    </div>
   );
 };
 
 export default VedioPlayer;
-
-
-
-  //   <div>
-  //     <p>A video!</p>
-  // <video src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3/h264.mov" controls></video>
-  // <p>That was a video.</p>
-  //   </div>
-//     <div className="player-wrapper">
-//  <div className="react-player"> <iframe  width="560" height="315" src="https://www.youtube.com/embed/Vbg81kc56FU" frameborder="0"
-//     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
- 
-//     </div>
-      // <div className="player-wrapper">
-      //   <ReactPlayer
-      //     className="react-player container"
-      //     url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
-      //     width="100%"
-      //     height="100%"
-      //   />
-      // </div>
